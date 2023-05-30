21c8085dccb478f86e6a092f85cc42470cfa858a
luisSpinola <luismspinola@gmail.com>
2023-05-30 14:22:13 +0100

    Minor changes

--- pages/aivf/utils/shared/options/sections/map/mapa.js
+++ pages/aivf/utils/shared/options/sections/map/mapa.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { OPTIONS_MAP, OPTIONS_MAP_PERMANENT_TOOLTIP, OPTIONS_MAP_PERMANENT_TOOLTIP_INFO, OPTIONS_MAP_PERMANENT_TOOLTIP_ORDER, OPTIONS_MAP_TILESET_SELECTION } from "../../../../localization/ptPt";
+import { OPTIONS_MAP, OPTIONS_MAP_PERMANENT_TOOLTIP, OPTIONS_MAP_PERMANENT_TOOLTIP_INFO, OPTIONS_MAP_PERMANENT_TOOLTIP_ORDER, OPTIONS_MAP_TILESET_SELECTION, OPTIONS_MAP_TILESET_SELECTION_NORMAL_1, OPTIONS_MAP_TILESET_SELECTION_NORMAL_2, OPTIONS_MAP_TILESET_SELECTION_NORMAL_3, OPTIONS_MAP_TILESET_SELECTION_NORMAL_4, OPTIONS_MAP_TILESET_SELECTION_SATELLITE, OPTIONS_MAP_TILESET_SELECTION_SIMPLE_1, OPTIONS_MAP_TILESET_SELECTION_SIMPLE_2, OPTIONS_MAP_TILESET_SELECTION_SIMPLE_3, OPTIONS_MAP_TILESET_SELECTION_SIMPLE_DARK_1, OPTIONS_MAP_TILESET_SELECTION_TERRAIN_1, OPTIONS_MAP_TILESET_SELECTION_TERRAIN_2, OPTIONS_MAP_TILESET_SELECTION_TERRAIN_3, OPTIONS_MAP_TILESET_SELECTION_TOPOLOGY_1, OPTIONS_MAP_TILESET_SELECTION_TOPOLOGY_2 } from "../../../../localization/ptPt";
 import { selectInput, switchInput } from "../components/inputs";
 import { getSectionStructure } from "../components/sectionStructure";
 
@@ -11,20 +11,20 @@ export const getMapOptions = (options, setOptions, {stickyTooltip, order}) => {
         <React.Fragment>
             {selectInput(OPTIONS_MAP_TILESET_SELECTION, "map_leaf", options, setOptions, false, 
                 [
-                    {value:0, name:"Normal"},
-                    {value:1, name:"Normal 2"},
-                    {value:2, name:"Normal 3"},
-                    {value:3, name:"Normal 4"},
-                    {value:4, name:"Topologia"},
-                    {value:5, name:"Topologia 2"},
-                    {value:6, name:"Simples"},
-                    {value:7, name:"Simples 2"},
-                    {value:8, name:"Simples 3"},
-                    {value:9, name:"Simples Escuro"},
-                    {value:10, name:"Terreno"},
-                    {value:11, name:"Terreno 2"},
-                    {value:12, name:"Terreno 3"},
-                    {value:13, name:"Satélite"},
+                    {value:0, name:OPTIONS_MAP_TILESET_SELECTION_NORMAL_1},
+                    {value:1, name:OPTIONS_MAP_TILESET_SELECTION_NORMAL_2},
+                    {value:2, name:OPTIONS_MAP_TILESET_SELECTION_NORMAL_3},
+                    {value:3, name:OPTIONS_MAP_TILESET_SELECTION_NORMAL_4},
+                    {value:4, name:OPTIONS_MAP_TILESET_SELECTION_TOPOLOGY_1},
+                    {value:5, name:OPTIONS_MAP_TILESET_SELECTION_TOPOLOGY_2},
+                    {value:6, name:OPTIONS_MAP_TILESET_SELECTION_SIMPLE_1},
+                    {value:7, name:OPTIONS_MAP_TILESET_SELECTION_SIMPLE_2},
+                    {value:8, name:OPTIONS_MAP_TILESET_SELECTION_SIMPLE_3},
+                    {value:9, name:OPTIONS_MAP_TILESET_SELECTION_SIMPLE_DARK_1},
+                    {value:10, name:OPTIONS_MAP_TILESET_SELECTION_TERRAIN_1},
+                    {value:11, name:OPTIONS_MAP_TILESET_SELECTION_TERRAIN_2},
+                    {value:12, name:OPTIONS_MAP_TILESET_SELECTION_TERRAIN_3},
+                    {value:13, name:OPTIONS_MAP_TILESET_SELECTION_SATELLITE},
             ]   )
             }
             {stickyTooltip && <React.Fragment><Divider style={{marginBottom:'0.5rem', marginTop:'0.5rem'}}/>{switchInput(OPTIONS_MAP_PERMANENT_TOOLTIP, "permanent_tooltips", options, setOptions, false)}</React.Fragment>}
